8d2a17c73911089d799e068d772cc92c3a8f71f8
Kazuhiro Kubota <k2.wanko@gmail.com>
2015-04-30 01:12:58 +0900

    Add gopath

--- go.sh
+++ go.sh
@@ -2,4 +2,5 @@ if [ -x "`which go`" ]; then
   export GOROOT="/usr/local/Cellar/go/$(brew info go | tr "\n" " " | awk '{print $3}')/libexec/"
 #$(brew info go | grep -o -e ^/[\/a-zA-Z0-9\.]\*)/libexec
   export GOPATH=$HOME/go
+  export PATH=$PATH:/usr/local/opt/go/libexec/bin
 fi
